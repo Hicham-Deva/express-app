c1838f34914adb8b530f0d408d99649ce0e7c3cd
hesahm_ab_git <hicham.abhoun@gmail.com>
2026-05-08 21:29:53 +0100

    complete CRUD: Add Update, Delete with Postgres DB, and env config

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,32 +1,66 @@
-let users = []
-exports.getUsers = (req, res) => {
-    res.json(users);
+const pool = require('../db');
+
+exports.getUsers = async(req, res) => {
+    try {
+        const result = await pool.query("select * from users order by created_at desc");
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'error fetching users' });
+    }
+}
+exports.createUser = async(req, res) => {
+    try {
+        const { name, email } = req.body;
+        const queryText = 'insert into users (name,email) values($1,$2) returning *';
+        const result = await pool.query(queryText, [name, email]);
+        res.status(201).json({ message: 'a new user has been added!', user: result.rows[0] })
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "no user has been added!" })
+    }
+
+}
+
+exports.updateUser = async(req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        console.log(id)
+        const { name, email } = req.body;
+        const queryText = 'UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *';
+        const result = await pool.query(queryText, [name, email, id]);
+        res.json({
+            message: 'User updated successfully',
+            user: result.rows[0]
+        });
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'no user updated' });
+
+    }
+
+}
+
+
+// 4. deleteUser (DELETE)
+exports.deleteUser = async(req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        const queryText = 'DELETE FROM users WHERE id = $1 RETURNING *';
+
+        const result = await pool.query(queryText, [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.json({ message: 'User deleted successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error deleting user' });
+    }
 };
-exports.createUser = (req, res) => {
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-        email: req.body.email
-    }
-    users.push(newUser);
-    res.status(201).json({
-        message: "new user has been added!",
-        data: newUser
-    })
-}
-
-exports.updateUser = (req, res) => {
-    const id = parseInt(req.params.id);
-    const userIndex = users.findIndex(user => user.id === id);
-    if (userIndex === -1) {
-        return res.status(404).json({ message: `The user not found` })
-    }
-    users[userIndex] = {...users[userIndex], ...req.body }
-    res.json({ message: "the user updated", user: users[userIndex] });
-}
-
-exports.deleteUser = (req, res) => {
-    const id = parseInt(req.params.id);
-    users = users.filter(user => user.id !== id);
-    res.status(203).json({ message: `the user ${id} deleted!` });
-}
